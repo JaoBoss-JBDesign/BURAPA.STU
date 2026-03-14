b518c214a449f907df4412615c8aa83ff445ce6f
JAOBOSS.TH <146152746+JaoBoss-JBDesign@users.noreply.github.com>
2026-03-14 16:23:47 +0700

    session.js

--- session.js
+++ session.js
@@ -1,11 +1,12 @@
 const SESSION_LIMIT = 15 * 60 * 1000
+
 const last = localStorage.getItem("last_active")
 
 if(last){
 
 const now = Date.now()
 
-if(now - last > SESSION_LIMIT){
+if(now - Number(last) > SESSION_LIMIT){
 
 localStorage.clear()
 
@@ -23,6 +24,7 @@ localStorage.setItem("last_active", Date.now())
 
 updateSession()
 
-document.addEventListener("click",updateSession)
-document.addEventListener("keydown",updateSession)
-document.addEventListener("touchstart",updateSession)
+document.addEventListener("click", updateSession)
+document.addEventListener("keydown", updateSession)
+document.addEventListener("touchstart", updateSession)
+document.addEventListener("input", updateSession)
